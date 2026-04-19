19c61305391bdbfcd206792497aee1a6cc979de6
Niels Klazenga <niels.klazenga@rbg.vic.gov.au>
2026-04-19 09:16:14 +0000

    Add versioning, blameable and unique index to entity_image_map table

--- database/migrations/2026_04_19_074439_create_entity_image_map_table.php
+++ database/migrations/2026_04_19_074439_create_entity_image_map_table.php
@@ -18,7 +18,18 @@ return new class extends Migration
             $table->foreignId('image_role_id')->constrained('controlled_terms');
             
             $table->integer('sort_order')->default(0);
-            $table->timestamps();
+
+            // Versioning, Blameable and Timestamps
+            $table->unsignedSmallInteger('version')->default(1);
+            $table->foreignId('created_by_id')->nullable()->constrained('agents');
+            $table->foreignId('updated_by_id')->nullable()->constrained('agents');
+            $table->timestampsTz();
+
+            $table->unique([
+                'entity_type', 
+                'entity_id', 
+                'image_id'
+            ], 'entity_image_unique');
         });
     }
 
